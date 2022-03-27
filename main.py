4f844ab1c4b77fc0bda8476a525d9b008c1134d4
Matteoverzotti <matteoalexandru2612@gmail.com>
2022-03-27 02:39:14 +0200

    Updated source files

--- main.py
+++ main.py
@@ -24,8 +24,12 @@ jax.random.PRNGKey(seed)
 
 import json
 import csv
+import random
 
-ROWS = 73276
+numberList = [1, 0, 8, 15, 4, 11]
+
+with open('Data/tag_to_id.json', 'r') as f:
+    tag_to_id = json.load(f)
 
 with open('Data/test.json', 'r') as fin:
     data = json.load(fin)
@@ -38,10 +42,12 @@ with open('submission1.csv', 'w') as fout:
     csv_row = 0
     for line in range(len(data)):
         for word in data[line]["tokens"]:
-            id = 0
+            id = random.choices(numberList, weights = (30, 20, 15, 15, 10, 10), k = 1)
             if word[0] >= '0' and word[0] <= '9':
                 id = 13 # NUMERIC
             elif word[0].isalpha() and word[0].isupper():
                 id = 1 # PERSON
+            else: id = id[0]
+
             writer.writerow([csv_row, id])
             csv_row = csv_row + 1
